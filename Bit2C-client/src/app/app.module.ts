5eff9dd3d51dead7a2387184987d794de40b13f7
Lev <lev@clrsky.io>
2018-05-17 23:44:03 +0300

    Added animations.

--- Bit2C-client/src/app/app.module.ts
+++ Bit2C-client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BackendService } from "./services/backend/backend.service";
@@ -9,7 +10,13 @@ import { TickerPickerComponent } from "./ticker-picker/ticker-picker.component";
 
 @NgModule({
   declarations: [AppComponent, TickerPickerComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule
+  ],
   providers: [BackendService],
   bootstrap: [AppComponent]
 })
--- Bit2C-client/src/app/ticker-picker/ticker-picker.component.ts
+++ Bit2C-client/src/app/ticker-picker/ticker-picker.component.ts
@@ -1,3 +1,10 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from "@angular/animations";
 import { Component, OnInit } from "@angular/core";
 import { BackendService } from "../services/backend/backend.service";
 import { TickerResponse } from "../services/backend/tickerResponse";
@@ -6,9 +13,25 @@ import { PairOption } from "./models/pairOption";
 @Component({
   selector: "app-ticker-picker",
   templateUrl: "./ticker-picker.component.html",
-  styleUrls: ["./ticker-picker.component.scss"]
+  styleUrls: ["./ticker-picker.component.scss"],
+  animations: [
+    trigger("flyInLeft", [
+      state("out", style({ transform: "translateX(-150%)" })),
+      state("in", style({ transform: "translateX(0)" })),
+      transition("out => in", [animate("0.3s 0.1s ease-in-out")]),
+      transition("in => out", [animate("0.3s 0.1s ease-in-out")])
+    ]),
+    trigger("flyInRight", [
+      state("out", style({ transform: "translateX(150%)" })),
+      state("in", style({ transform: "translateX(0)" })),
+      transition("* => in", [animate("0.3s 0.1s ease-in-out")]),
+      transition("* => out", [animate("0.3s 0.1s ease-in-out")])
+    ])
+  ]
 })
 export class TickerPickerComponent implements OnInit {
+  public state: string = "out";
+
   public pairs: PairOption[] = [
     new PairOption(-1, "Please select a coin..."),
     new PairOption(0, "BTC", "BtcNis"), // Lev: The coins and the pairs should be retreived from the server, but the task was to hard-code the pairs...
@@ -34,12 +57,21 @@ export class TickerPickerComponent implements OnInit {
         (data: TickerResponse) => {
           this.tickerData = data;
           this.isFetching = false;
+          this.state = "in";
         },
         error => {
           this.isFetching = false;
           // more error handling goes here....
         }
       );
+    } else {
+      this.state = "out";
+    }
+  }
+
+  public onAnimationFinished(): void {
+    if (this.state === "out") {
+      this.tickerData = null; // If we would reset tickerData before animation finishes, the elements would disappear without the animation...
     }
   }
 }
